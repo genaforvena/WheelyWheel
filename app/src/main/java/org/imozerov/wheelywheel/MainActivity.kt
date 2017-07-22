bc19235e286fb1d9d14ac01a6badea0ffb9955c1
Ilya Mozerov <ilyailiilia@gmail.com>
2017-07-22 23:18:44 +0300

    Initial commit

---
+++ app/src/main/java/org/imozerov/wheelywheel/MainActivity.kt
@@ -0,0 +1,75 @@
+package org.imozerov.wheelywheel
+
+import android.arch.lifecycle.LifecycleActivity
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.os.Handler
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : LifecycleActivity() {
+    private val handler = Handler()
+
+    private lateinit var viewModel : MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        viewModel.wheelValue().observe(this, Observer<Int> { wheelValue ->
+            wheel.text = wheelValue.toString()
+        })
+
+        viewModel.mode().observe(this, Observer { mode ->
+            when (mode) {
+                Mode.TIMER -> {
+                    timer.isChecked = true
+                    number_to_show.visibility = View.GONE
+                }
+                Mode.EDIT -> {
+                    pick_number.isChecked = true
+                    number_to_show.visibility = View.VISIBLE
+                }
+                Mode.RANDOM -> {
+                    random_number.isChecked = true
+                    number_to_show.visibility = View.GONE
+                }
+            }
+        })
+
+        timer.setOnCheckedChangeListener { _, isChecked -> if (isChecked) viewModel.setMode(Mode.TIMER) }
+        pick_number.setOnCheckedChangeListener { _, isChecked -> if (isChecked) {
+            viewModel.setMode(Mode.EDIT)
+            viewModel.safelySetNumberOrIgnore(number_to_show.text)
+        }}
+        random_number.setOnCheckedChangeListener { _, isChecked -> if (isChecked) viewModel.setMode(Mode.RANDOM) }
+
+        number_to_show.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {}
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                handler.removeCallbacksAndMessages(null)
+                handler.postDelayed({
+                    viewModel.safelySetNumberOrIgnore(p0)
+                }, 300)
+            }
+        })
+    }
+
+    private fun MainViewModel.safelySetNumberOrIgnore(numberStr: CharSequence?) {
+        if (number_to_show.visibility != View.VISIBLE) {
+            return
+        }
+        numberStr?.let {
+            if (!it.isBlank()) {
+                viewModel.setValue(Integer.parseInt(numberStr.toString()))
+            }
+        }
+    }
+}
---
+++ app/src/main/java/org/imozerov/wheelywheel/MainViewModel.kt
@@ -0,0 +1,78 @@
+package org.imozerov.wheelywheel
+
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.os.Handler
+import java.util.*
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicBoolean
+
+class MainViewModel(application: Application): AndroidViewModel(application) {
+    private val wheelValue = MutableLiveData<Int>()
+    private val mode = MutableLiveData<Mode>()
+
+    private val handler = Handler()
+
+    private val stopHandler = AtomicBoolean(true)
+    private val incrementor = object : Runnable {
+        override fun run() {
+            if (stopHandler.get()) {
+                return
+            }
+
+            val value = if (wheelValue.value != null) wheelValue.value as Int + 1 else 0
+            wheelValue.postValue(value)
+
+            if (!stopHandler.get()) {
+                handler.postDelayed(this, TimeUnit.SECONDS.toMillis(1))
+            }
+        }
+    }
+
+    init {
+        mode.value = Mode.RANDOM
+    }
+
+    fun wheelValue() : LiveData<Int> = wheelValue
+
+    fun mode() : LiveData<Mode> = mode
+
+    fun setValue(value: Int) {
+        if (mode.value != Mode.EDIT) {
+            throw RuntimeException("Please set mode to edit first!")
+        }
+        wheelValue.value = value
+    }
+
+    fun setMode(newMode: Mode) {
+        if (mode.value == newMode) {
+            return
+        }
+
+        mode.value = newMode
+
+        when (newMode) {
+            Mode.EDIT -> {
+                stopHandler.set(true)
+            }
+            Mode.RANDOM -> {
+                stopHandler.set(true)
+                wheelValue.value = Random().nextInt(LIMIT)
+            }
+            Mode.TIMER -> {
+                stopHandler.set(false)
+                handler.post(incrementor)
+            }
+        }
+    }
+
+    companion object {
+        private val LIMIT = 99999
+    }
+}
+
+enum class Mode {
+    EDIT, RANDOM, TIMER
+}
